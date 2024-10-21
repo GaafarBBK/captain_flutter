6a00fb1ae3135428830ee67c43361ecd4fc14da7
ABD2005LY <bdwm83249@gmail.com>
2024-10-21 10:58:05 +0200

    الاثنين

--- lib/screen/bottm_navigation/StorePage.dart
+++ lib/screen/bottm_navigation/StorePage.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:captain11/widget/cards.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:captain11/screen/auth_screens/login.dart';
 import 'package:captain11/screen/bottm_navigation/HomePage.dart';
 import 'package:captain11/screen/bottm_navigation/TodayPage.dart';
 
 class StorePage extends StatefulWidget {
   const StorePage({super.key});
   @override
-  // ignore: library_private_types_in_public_api
   _StorePageState createState() => _StorePageState();
 }
 
@@ -40,24 +41,104 @@ class _StorePageState extends State<StorePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ملفي '),
-      ),
-      body: const Center(
+      body: Padding(
+        padding: const EdgeInsets.all(0.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: NetworkImage('https://example.com/user.jpg'),
+            Row(
+              children: [
+                const SizedBox(width: 10),
+                Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    Container(
+                      // decoration: BoxDecoration(
+                      //   color: Colors.blue,
+                      //   shape: BoxShape.circle,
+                      // ),
+                      padding: const EdgeInsets.all(10),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: Colors.blue,
+                            width: 5,
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.5),
+                              spreadRadius: 2,
+                              blurRadius: 10,
+                              offset: Offset(0, 5),
+                            ),
+                          ],
+                        ),
+                        child: const CircleAvatar(
+                          radius: 65,
+                          backgroundImage: AssetImage('assets/login.png'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'عبد الرحمن رشدي',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '0913362358',
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.black54),
+                    ),
+                    Text(
+                      'bdwm83249@gmail.com',
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.black54),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            SizedBox(height: 20),
-            Text(
-              'علي أحمد',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            const SizedBox(height: 30),
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.all(3.0),
+              ),
             ),
-            SizedBox(height: 10),
-            Text('مطور تطبيقات فلاتر'),
+            const SizedBox(height: 20),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => TodayPage()),
+                );
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(color: Colors.black),
+                  color: Colors.blue,
+                ),
+                height: 50,
+                width: 300,
+                alignment: Alignment.center,
+                child: Text(
+                  'تدريب اليوم',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
           ],
         ),
       ),
@@ -65,11 +146,48 @@ class _StorePageState extends State<StorePage> {
   }
 }
 
+// class ProfileDetailRow extends StatelessWidget {
+//   final String title;
+//   final String value;
+
+//   const ProfileDetailRow({
+//     required this.title,
+//     required this.value,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(vertical: 8.0),
+//       child: Row(
+//         children: [
+//           const SizedBox(width: 16),
+//           Text(
+//             title,
+//             style: const TextStyle(
+//               fontSize: 18,
+//               fontWeight: FontWeight.bold,
+//             ),
+//           ),
+//           const Spacer(),
+//           Text(
+//             value,
+//             style: TextStyle(
+//               fontSize: 18,
+//               color: Colors.grey[700],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 PreferredSizeWidget StoreAppBar() {
   return AppBar(
     centerTitle: true,
     title: Text(
-      ' ',
+      '',
       style: GoogleFonts.changa(
         fontSize: 24,
         fontWeight: FontWeight.bold,
